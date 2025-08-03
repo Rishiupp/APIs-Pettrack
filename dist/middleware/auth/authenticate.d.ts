053ce25473bfcedfeb8291332d6eb6890a7f1a82
Rishiupp <rishiupadhayay67@gmail.com>
2025-08-03 18:03:51 +0000

    dist folder debug1

--- dist/middleware/auth/authenticate.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response, NextFunction } from 'express';
-import { AuthRequest } from '../../types';
-export declare const authenticate: (req: AuthRequest, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=authenticate.d.ts.map
